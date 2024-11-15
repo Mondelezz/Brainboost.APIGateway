c55e156db0d411bf0c5a34209615f99aed0bfb4b
Egor Pankov <pankov.egor26032005@yandex.ru>
2024-11-15 19:58:59 +0300

    first configuration

---
+++ API/Program.cs
@@ -0,0 +1,30 @@
+﻿using API;
+using Ocelot.Middleware;
+using Serilog;
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
+
+Log.Information($"Starting application: {typeof(Program).Assembly.GetName().Name}");
+
+try
+{
+    var builder = WebApplication.CreateBuilder(args);
+
+    await builder
+        .ConfigureServices()
+        .Build()
+        .ConfigurePipeline()
+        .RunAsync();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Unhandled exception");
+}
+finally
+{
+    Log.Information("Shut down complete");
+    Log.CloseAndFlush();
+}
+
